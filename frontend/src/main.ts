7eb802fb23cd64deeb3e143722e632b8e50816e0
Juan Pablo PRADA MEJIA <jpradamejia@ncelrnd2906.nce.amadeus.net>
2024-07-03 15:29:03 +0200

    frontend working, still need to be fixed but, good enough

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import { init } from "./ts/games/reaction_game";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div class="wrapper">
     <canvas id="myCanvas"></canvas>
+    <button class="gameButton"> Play </button>
     <div>
 `;
-
-init()
+init();
